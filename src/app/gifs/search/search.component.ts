844d9ac3fd43a14d9453534a89658c5dd1a50364
Christian GUEVARA <christian.guevara@exakis-nelite.com>
2022-08-04 15:14:56 +0200

    Modularize layout in components. Add a shared and a gifs modules

---
+++ src/app/gifs/search/search.component.ts
@@ -0,0 +1,15 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent {
+
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
+  
+  search() {
+    console.log(this.txtSearch.nativeElement.value);
+    this.txtSearch.nativeElement.value = '';
+  }
+}
