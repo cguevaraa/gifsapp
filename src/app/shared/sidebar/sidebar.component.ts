2fd490c65b47d568b9689a67701e055099d709ba
Christian GUEVARA <christian.guevara@exakis-nelite.com>
2022-08-05 22:29:15 +0200

    Add custom icons

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -4,6 +4,8 @@ import { GifsService } from 'src/app/gifs/services/gifs.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+
 })
 export class SidebarComponent {
 
