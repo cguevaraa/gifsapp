7597b3cfee29a5323c2daf7611c3762e6f07f244
Christian GUEVARA <christian.guevara@exakis-nelite.com>
2022-08-05 21:23:23 +0200

    Add HttpParams object to manage request parameters

--- src/app/gifs/services/search.service.ts
+++ src/app/gifs/services/search.service.ts
@@ -3,7 +3,7 @@ Builds and sends the http request and fetch
 the data in the response.
 */ 
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import connection from '../../connection/giphy.json' //Import here your own json with your (Giphy) API key
@@ -14,16 +14,20 @@ import { Gif, SearchGIFResponse } from '../interfaces/gifs.interfaces';
 })
 export class SearchService {
 
-  private _url: string = 'https://api.giphy.com/v1/gifs/search';
+  private _urlService: string = 'https://api.giphy.com/v1/gifs';
   private _limit: number = 10;
 
   constructor(private http: HttpClient) { }
 
   search(query: string): Observable<Gif[]> {
-    const subject = new Subject<Gif[]>();
-    const request = `${this._url}?api_key=${connection.key}&q=${query}&limit=${this._limit}`;
 
-    this.http.get<SearchGIFResponse>(request)
+    const params: HttpParams = new HttpParams()
+      .set('api_key', connection.key)
+      .set('limit', this._limit)
+      .set('q', query);
+
+    const subject = new Subject<Gif[]>();
+    this.http.get<SearchGIFResponse>(`${this._urlService}/search`, {params})
       .subscribe(res => {
         subject.next(res.data);
         subject.complete();
